50eb548a9f2921cec0d4c3595b487b6491f9bb72
PavloMuk <pmykalov@gmail.com>
2024-06-15 09:36:41 +0300

    Fixes

--- ad8302/device.py
+++ ad8302/device.py
@@ -9,14 +9,17 @@ import re
 
 @dataclasses.dataclass(init=False)
 class ParseResult:
+    t: float
     vphs: np.ndarray
     vmag: np.ndarray
 
 
 def parse(s: str) -> ParseResult:
     res = ParseResult()
+    t_matches = re.findall(r'T:\s*(-?\d*\.\d+|-?\d+\.?)', s)
     vmag_matches = re.findall(r'VMAG[0-3]:\s*(-?\d*\.\d+|-?\d+\.?)', s)
     vphs_matches = re.findall(r'VPHS[0-3]:\s*(-?\d*\.\d+|-?\d+\.?)', s)
+    res.t = float(t_matches[0])
     res.vmag = np.array([float(val) for val in vmag_matches])
     res.vphs = np.array([float(val) for val in vphs_matches])
     if not len(res.vphs) == len(res.vmag) == 4:
@@ -37,6 +40,8 @@ def v_to_mag(vol: np.ndarray) -> np.ndarray:
 class Device:
     def __init__(self, path_id: str):
         self.serial = serial.Serial(path_id)
+        self.serial.baudrate = 115200
+        self.serial.ReadBufferSize = 1
 
     def spin(self, callback: Callable[[float, ParseResult], None]):
         while True:
@@ -46,6 +51,4 @@ class Device:
             except (ValueError, IndexError, RuntimeError):
                 print("Error parsing data")
                 continue
-            t = time.time()
-            print(f"{t}: {arr}")
-            callback(t, arr)
+            callback(arr.t/1000, arr)
--- ad8302/show_beamforming.py
+++ ad8302/show_beamforming.py
@@ -9,8 +9,9 @@ from beamformer import Beamformer
 
 
 def main():
-    freq = 0.450e9
-    d_to_ref = np.array([-0.205, -0.104, 0.100, 0.202])
+    freq = 0.433e9
+    d_to_ref = np.array([-0.2, -0.1, 0.1, 0.2])
+    d_to_ref = np.array([-0.2, -0.1, 0.1, 0.2])
     beamformer = Beamformer(freq, d_to_ref)
     dev = Device('/dev/ttyACM0')
 
@@ -18,27 +19,38 @@ def main():
     plt.grid(True)
 
     num_ant = 4
-    ax1 = plt.subplot(311)
+
+    ax1 = plt.subplot(411, projection='polar')
+    ax1.set_theta_zero_location('N')
+    ax1.set_theta_direction(-1)
+    ax1.set_rlabel_position(55)
+    ax1.set_xlim([-180, +180])
+    ax1.set_ylim([0, 1.1])
+    beamloss_polar, = ax1.plot([], [], lw=3, label='beamloss')
+    mins_polar, = ax1.plot([], [], lw=3, label='mins')
+
+
+    ax2 = plt.subplot(412)
     # ax1.set_theta_zero_location('N')
     # ax1.set_theta_direction(-1)
     # ax1.set_rlabel_position(55)
-    ax1.set_xlim([-180, +180])
-    ax1.set_ylim([0, 1.1])
-    beamloss, = ax1.plot([], [], lw=3, label='beamloss')
-    mins, = ax1.plot([], [], lw=3, label='mins', marker='o')
+    ax2.set_xlim([-180, +180])
+    ax2.set_ylim([0, 1.1])
+    beamloss, = ax2.plot([], [], lw=3, label='beamloss')
+    mins, = ax2.plot([], [], lw=3, label='mins', marker='o')
 
-    ax2 = plt.subplot(312)
-    ax2.set_ylim([0.0, 180.0])
+    ax3 = plt.subplot(413)
+    ax3.set_ylim([-10.0, 180.0])
     phs_lines = []
     for i in range(num_ant):
-        line, = ax2.plot([], lw=3, label=f'ant{i}')
+        line, = ax3.plot([], lw=3, label=f'ant{i}')
         phs_lines.append(line)
 
-    ax3 = plt.subplot(313)
-    ax3.set_ylim([-35, +35])
+    ax4 = plt.subplot(414)
+    ax4.set_ylim([-35, +35])
     mag_lines = []
     for i in range(num_ant):
-        line, = ax3.plot([], lw=3, label=f'ant{i}')
+        line, = ax4.plot([], lw=3, label=f'ant{i}')
         mag_lines.append(line)
 
     plt.legend()
@@ -60,11 +72,13 @@ def main():
         mns_y = errors[np.argsort(errors)[:2]]
         mins.set_xdata(mns_x)
         mins.set_ydata(mns_y)
-        print(mns_x[:5])
+        beamloss_polar.set_xdata(np.deg2rad(doas))
+        beamloss_polar.set_ydata(errors)
+
         plt.grid(True)
         time_data.append(t)
-        ax2.set_xlim(max(time_data) - 10.0, max(time_data) + 1.0)
         ax3.set_xlim(max(time_data) - 10.0, max(time_data) + 1.0)
+        ax4.set_xlim(max(time_data) - 10.0, max(time_data) + 1.0)
 
         phase_data.append(phases)
         mag_data.append(v_to_mag(parse.vmag))
